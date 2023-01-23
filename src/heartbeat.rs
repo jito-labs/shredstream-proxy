9b9263cb0ff7783ccf30edf0c3e72bf005d9cd19
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-01-23 10:14:47 -0800

    add extra loop

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -38,9 +38,6 @@ pub fn heartbeat_loop_thread(
         .spawn(move || {
             let mut successful_heartbeat_count: u64 = 0;
             let mut failed_heartbeat_count: u64 = 0;
-            let mut shredstream_client = runtime
-                .block_on(get_grpc_client(&block_engine_url, &auth_keypair))
-                .expect("failed to connect to block engine");
             let (mut heartbeat_interval, mut failed_heartbeat_interval) = (Duration::from_millis(100), Duration::from_millis(100)); //start with 100ms, change based on server suggestion
             let heartbeat_socket = jito_protos::shared::Socket {
                 ip: recv_socket.ip().to_string(),
@@ -48,50 +45,63 @@ pub fn heartbeat_loop_thread(
             };
 
             while !exit.load(Ordering::Relaxed) {
-                let start = Instant::now();
-                let heartbeat_result = runtime.block_on(shredstream_client
-                    .send_heartbeat(Heartbeat {
-                        socket: Some(heartbeat_socket.clone()),
-                        regions: regions.clone(),
-                    }));
+                let shredstream_client = runtime
+                    .block_on(get_grpc_client(&block_engine_url, &auth_keypair));
 
-                match heartbeat_result {
-                    Ok(x) => {
-                        let new_ttl = x.get_ref().ttl_ms as u64;
-                        let new_interval = Duration::from_millis(new_ttl.checked_div(2).unwrap());
-                        if heartbeat_interval != new_interval {
-                            info!("Setting heartbeat interval to {new_interval:?}.");
-                            heartbeat_interval = new_interval;
-                            failed_heartbeat_interval = Duration::from_millis(new_ttl / 4);
+                let mut shredstream_client = match shredstream_client {
+                    Ok(c) => c,
+                    Err(e) => {
+                        warn!("Failed to connect to block engine, {e}");
+                        continue;
+                    }
+                };
+                while !exit.load(Ordering::Relaxed) {
+                    let start = Instant::now();
+                    let heartbeat_result = runtime.block_on(shredstream_client
+                        .send_heartbeat(Heartbeat {
+                            socket: Some(heartbeat_socket.clone()),
+                            regions: regions.clone(),
+                        }));
+
+                    match heartbeat_result {
+                        Ok(x) => {
+                            let new_ttl = x.get_ref().ttl_ms as u64;
+                            let new_interval = Duration::from_millis(new_ttl.checked_div(2).unwrap());
+                            if heartbeat_interval != new_interval {
+                                info!("Setting heartbeat interval to {new_interval:?}.");
+                                heartbeat_interval = new_interval;
+                                failed_heartbeat_interval = Duration::from_millis(new_ttl / 4);
+                            }
+                            successful_heartbeat_count += 1;
+
+                            let elapsed = start.elapsed();
+                            if elapsed.lt(&heartbeat_interval) {
+                                sleep(heartbeat_interval.sub(elapsed));
+                            }
                         }
-                        successful_heartbeat_count += 1;
+                        Err(err) => {
+                            if err.code() == Code::InvalidArgument {
+                                exit.store(true, Ordering::SeqCst);
+                                error!("Invalid arguments: {err}");
+                                return;
+                            };
+                            warn!("Error sending heartbeat: {err}");
+                            datapoint_warn!("heartbeat_send_error", ("errors", 1, i64));
+                            failed_heartbeat_count += 1;
 
-                        let elapsed = start.elapsed();
-                        if elapsed.lt(&heartbeat_interval) {
-                            sleep(heartbeat_interval.sub(elapsed));
+                            // sleep faster to avoid getting deleted via TTL expiration in NATS
+                            let elapsed = start.elapsed();
+                            if elapsed.lt(&failed_heartbeat_interval) {
+                                sleep(failed_heartbeat_interval.sub(elapsed));
+                            }
                         }
                     }
-                    Err(err) => {
-                        if err.code() == Code::InvalidArgument {
-                            exit.store(true, Ordering::SeqCst);
-                            error!("Invalid arguments: {err}");
-                            return;
-                        };
-                        warn!("Error sending heartbeat: {err}");
-                        datapoint_warn!("heartbeat_send_error", ("errors", 1, i64));
-                        failed_heartbeat_count += 1;
-
-                        // sleep faster to avoid getting deleted via TTL expiration in NATS
-                        let elapsed = start.elapsed();
-                        if elapsed.lt(&failed_heartbeat_interval) {
-                            sleep(failed_heartbeat_interval.sub(elapsed));
-                        }
+                    let elapsed = start.elapsed();
+                    if elapsed.lt(&heartbeat_interval) {
+                        sleep(heartbeat_interval.sub(elapsed));
                     }
                 }
-                let elapsed = start.elapsed();
-                if elapsed.lt(&heartbeat_interval) {
-                    sleep(heartbeat_interval.sub(elapsed));
-                }
+                sleep(Duration::from_millis(200)); // back off for a bit
             }
             info!("Exiting heartbeat thread, sent {successful_heartbeat_count} successful, {failed_heartbeat_count} failed shreds.");
         })
