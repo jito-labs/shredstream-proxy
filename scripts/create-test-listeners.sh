10726904909414ab114b6b7bb9ae1811e6061cf9
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-01-31 01:21:25 -0800

    update

--- scripts/create-test-listeners.sh
+++ scripts/create-test-listeners.sh
@@ -6,9 +6,9 @@ set -eu
 pkill busybox
 
 # start new listeners
-nohup ./busybox nc -v -u -l -p 9900 > 9900 &
-nohup ./busybox nc -v -u -l -p 9901 > 9901 &
-nohup ./busybox nc -v -u -l -p 9902 > 9902 &
+nohup ./busybox nc -v -u -l -p 9900 > 9900.out &
+nohup ./busybox nc -v -u -l -p 9901 > 9901.out &
+nohup ./busybox nc -v -u -l -p 9902 > 9902.out &
 
 # let socket show up
 sleep 0.5
--- scripts/get_tvu_port.sh
+++ scripts/get_tvu_port.sh
@@ -14,8 +14,7 @@ if [ ! -x "$(command -v curl)" ]; then
     exit 1
 fi
 
-
 IDENTITY_PUBKEY=$(curl "$HOST" -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getIdentity"}' | jq -r .result.identity )
 TPU_PORT=$(curl "$HOST" -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getClusterNodes"}' | jq -r ".result | map(select(.pubkey == \"$IDENTITY_PUBKEY\")) | .[0].tpu" )
 
-echo TVU Port to use with Shredstream: $TPU_PORT
+echo TVU Port to use with Shredstream: "$TPU_PORT"
