c4345ad696ddea0d8f04e460e461cbf9611c3370
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-02-02 03:38:23 -0800

    update script

--- scripts/get_tvu_port.sh
+++ scripts/get_tvu_port.sh
@@ -2,19 +2,20 @@
 set -eu
 
 # check jq exists
-if [ ! -x "$(command -v jq)" ]; then
-    echo "'jq' not found"
-    exit 1
+if [[ ! -x "$(command -v jq)" ]]; then
+  echo "'jq' not found"
+  exit 1
 fi
 
 # check curl exists
-if [ ! -x "$(command -v curl)" ]; then
-    echo "'curl' not found"
-    exit 1
+if [[ ! -x "$(command -v curl)" ]]; then
+  echo "'curl' not found"
+  exit 1
 fi
 
 HOST=${1:-"http://localhost:8899"}
-IDENTITY_PUBKEY=$(curl "$HOST" -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getIdentity"}' | jq -r .result.identity )
-TPU_PORT=$(curl "$HOST" -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getClusterNodes"}' | jq -r ".result | map(select(.pubkey == \"$IDENTITY_PUBKEY\")) | .[0].tpu" )
+IDENTITY_PUBKEY=$(curl --show-error --silent "$HOST" -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getIdentity"}' | jq -r .result.identity)
+TPU_PORT=$(curl --show-error --silent "$HOST" -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0", "id":1, "method":"getClusterNodes"}' | jq -r ".result | map(select(.pubkey == \"$IDENTITY_PUBKEY\")) | .[0].tpu")
 
-echo TVU Port to use with Shredstream: "$TPU_PORT"
+echo TVU Port to use with Shredstream:
+echo "$TPU_PORT"
