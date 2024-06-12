a79424537cf9d14263e827d4c3447d63626667a1
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2024-06-11 17:07:24 -0700

    Reduce number of cores

--- proxy/src/forwarder.rs
+++ proxy/src/forwarder.rs
@@ -51,7 +51,7 @@ pub fn start_forwarder_threads(
     exit: Arc<AtomicBool>,
 ) -> Vec<JoinHandle<()>> {
     let num_threads = num_threads
-        .unwrap_or_else(|| usize::from(std::thread::available_parallelism().unwrap()).max(8));
+        .unwrap_or_else(|| usize::from(std::thread::available_parallelism().unwrap()).max(4));
 
     let recycler: PacketBatchRecycler = Recycler::warmed(100, 1024);
 
